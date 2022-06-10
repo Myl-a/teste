425c3ee7701bbbffd4f3fcde192bb03654eed007
Sâmyla <samylamaria14@gmail.com>
2022-06-10 13:55:21 -0300

    testando git

---
+++ teste/src/main/java/com/ifce/br/model/Pessoa.java
@@ -0,0 +1,41 @@
+package com.ifce.br.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	private String time;
+	
+	private String nome;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
